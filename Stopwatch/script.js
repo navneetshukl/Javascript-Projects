b4beb7755232b85b6a902d86837403ea0ac52919
Navneet shukla <navneetshukla824@gmail.com>
2023-10-01 22:48:58 +0530

    Completed the javascript function for stopwatch

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -1,23 +1,55 @@
+var hour = 0;
+var minute = 0;
+var second = 0;
 
-    var hour = 0;
-    var minute = 0;
-    var second = 0 ;
+var appendSecond = document.getElementById("second");
+var appendMinute = document.getElementById("minute");
+var appendHour = document.getElementById("hour");
 
-    var appendSecond=document.getElementById("second")
-    var appendMinute=document.getElementById("minute")
-    var appendHour=document.getElementById("hour")
+var startButton = document.getElementById("start");
+var stopButton = document.getElementById("stop");
+var resetButton = document.getElementById("reset");
 
-    var start=document.getElementById("start")
-    var stop=document.getElementById("stop")
-    var reset=document.getElementById("reset")
+var timerInterval;
+var isRunning = false;
 
-    time=0;
-
-    function startTimer(){
-
-        time++
-        appendSecond.innerHTML=time
-
+function startTimer() {
+    second++;
+    if (second >= 60) {
+        second = 0;
+        minute++;
+        if (minute >= 60) {
+            minute = 0;
+            hour++;
+        }
     }
-    start.onclick=setInterval(startTimer,1000)
 
+    appendSecond.innerHTML = second;
+    appendMinute.innerHTML = minute;
+    appendHour.innerHTML = hour;
+}
+
+startButton.onclick = function () {
+    if (!isRunning) {
+        timerInterval = setInterval(startTimer, 1000);
+        isRunning = true;
+    }
+};
+
+stopButton.onclick = function () {
+    if (isRunning) {
+        clearInterval(timerInterval);
+        isRunning = false;
+    }
+};
+
+resetButton.onclick = function () {
+    clearInterval(timerInterval);
+    hour = 0;
+    minute = 0;
+    second = 0;
+    appendSecond.innerHTML = second;
+    appendMinute.innerHTML = minute;
+    appendHour.innerHTML = hour;
+    isRunning = false;
+};
